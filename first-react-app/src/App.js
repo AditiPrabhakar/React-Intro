945e58fbb459bb28b3ce674a62d6ee6c8c214d37
Aditi Prabhakar <aditiprabhakar04@gmail.com>
2024-11-02 15:46:20 +0000

    Hooks and Props

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import ClassComponent from './components/ClassComponent';
 import FunctionComponent from './components/FunctionComponent';
+import { useState } from 'react';
 
 function App() {
+  const author = "Aditi";
+  const [name, setName] = useState("Star");
+
   return (
     <div className="App">
       
@@ -11,7 +15,8 @@ function App() {
       <br></br>
 
       <p>Function Components</p>
-      <FunctionComponent />
+      {/* <FunctionComponent name = 'Star' age = {20} author = {author} /> */}
+      <FunctionComponent name = {name} age = {20} author = {author} setName = {setName} ></FunctionComponent>
     </div>
   );
 }
--- first-react-app/src/components/FunctionComponent.jsx
+++ first-react-app/src/components/FunctionComponent.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-const FunctionComponent = () => {
+const FunctionComponent = ({name, age, author, setName}) => { //* Props are the parameters containing information
+    const [count, setCount] = useState(0);
+    const [changeName, setChangeName] = useState("");
+    const reduceCount = () => {
+        setCount(count - 1);
+    }
     return(
         <div>
             <p>This is Function Component</p>
+            <button onClick={() => setCount(count + 1)}>Click here to increment by 1</button>
+            <hr></hr>
+            <button onClick={reduceCount}>Click here to decrement by 1</button>
+            <h2>{count}</h2>
+            <h4>My company is: {name}. It is {age} years old, established by {author}. </h4>
+            <input onChange={(e) => setChangeName(e.target.value)} />
+            <button onClick={() => setName(changeName)}>Change name to...</button>
         </div>
     )
 }
 
 export default FunctionComponent;
+
+// props: {
+//     name: "Star",
+//     age: 20,
+//     author: "Aditi"
+// }
