e203e0d0407f483ca647482f1df73fc5c0faf955
Aditi Prabhakar <aditiprabhakar04@gmail.com>
2024-11-03 06:59:09 +0000

    Routing and Layouts

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,24 +1,42 @@
 import './App.css';
-import ClassComponent from './components/ClassComponent';
-import FunctionComponent from './components/FunctionComponent';
+// import ClassComponent from './component/ClassComponent';
+import ClassComponent from './pages/ClassComponent';
+// import FunctionComponent from './component/FunctionComponent';
+import FunctionComponent from './pages/FunctionComponent';
 import { useState } from 'react';
+import { Routes, Route} from 'react-router-dom';
+//* HOC Layout
+import BaseHoc from './hoc/BaseHoc';
 
 function App() {
   const author = "Aditi";
   const [name, setName] = useState("Star");
-
+  
   return (
-    <div className="App">
-      
-      <p>Class Components</p>
-      <ClassComponent />
-      <br></br>
+    <Routes>
+      <Route path='/' element={<BaseHoc><ClassComponent /></BaseHoc>} />
+      <Route path='/functional-component' element={<BaseHoc><FunctionComponent 
+      name={name}
+      age={20}
+      author={author}
+      setName={setName}
+      /></BaseHoc>} />
+    </Routes>
+  )
 
-      <p>Function Components</p>
-      {/* <FunctionComponent name = 'Star' age = {20} author = {author} /> */}
-      <FunctionComponent name = {name} age = {20} author = {author} setName = {setName} ></FunctionComponent>
-    </div>
-  );
+  // return (
+    // <div className="App">
+      
+    //   <p>Class Components</p>
+    //   <ClassComponent />
+    //   <br></br>
+
+    //   <p>Function Components</p>
+    //   {/* <FunctionComponent name = 'Star' age = {20} author = {author} /> */}
+    //   <FunctionComponent name = {name} age = {20} author = {author} setName = {setName} ></FunctionComponent>
+    // </div>
+
+  // );
 }
 
 export default App;
